3181f74b6e8adf883230582bc31b237791661cf1
rhonep6134-arch <rhonep6134@student.faytechcc.edu>
2026-02-22 00:40:22 -0500

    Add files via upload

---
+++ P1Lab1.py
@@ -0,0 +1,9 @@
+# Phillip Rhone
+# 2/22/2026
+# P1Lab1.py
+# input and print
+
+first_name = input("Eneter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name + "!","Welcome to CTI-110")
